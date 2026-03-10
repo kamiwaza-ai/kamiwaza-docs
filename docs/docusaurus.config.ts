8b05d14d834c40bb757a4f0b4253c1d399ea5dfd
jonathanleff <jonathan@kamiwaza.ai>
2026-03-10 11:24:15 -0600

    merge dev to release/0.11.0

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -159,6 +159,21 @@ const config: Config = {
 				},
 			},
 		],
+		// Extensions docs plugin
+		[
+			"@docusaurus/plugin-content-docs",
+			{
+				id: "extensions",
+				path: "extensions",
+				routeBasePath: "extensions",
+				sidebarPath: require.resolve("./sidebars-extensions.ts"),
+				versions: {
+					current: {
+						label: "Latest",
+					},
+				},
+			},
+		],
 		// Research docs plugin
 		[
 			"@docusaurus/plugin-content-docs",
